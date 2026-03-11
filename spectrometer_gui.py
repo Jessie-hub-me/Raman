82df50add96b499f776c35ee72eb8e13c9d50b95
Jessie-hub-me <jessiejiaqizhang2024@gmail.com>
2026-03-11 12:11:42 -0700

    Add files via upload

--- spectrometer_gui.py
+++ spectrometer_gui.py
@@ -343,3 +343,18 @@ class SpectrometerGUI(QWidget):
 
             self.device.close()
             event.accept()
+            
+            
+            
+            
+if __name__ == "__main__":
+
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+
+    window = SpectrometerGUI()
+    window.show()
+
+    sys.exit(app.exec_())
